dd2ae17829eeb6e1bde924d7d8b0e5bc04abc776
Kresqle <75183858+Kresqle@users.noreply.github.com>
2021-04-28 16:14:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 // Declare consts
-const fs = require('fs')
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const { prefix, token } = require('./config.json')
-const { forbiddenWords } = require('./forbiddenWords.json')
-
-console.log(forbiddenWords)
+const fs = require("fs");
+const Discord = require("discord.js");
+require("discord-reply");
+const client = new Discord.Client();
+const { prefix, token } = require("./config.json");
+const { forbiddenWords } = require("./forbiddenWords.json");
 
 // setups the commands
-client.commands = new Discord.Collection()
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  client.commands.set(command.name, command)
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 // message event
-client.on('message', message => {
-  // delete a message if it contains a forbidden word
-  forbiddenWords.forEach(word => {
-    if (message.content.toLowerCase().includes(word)) {
-      message.delete()
-      message.channel.send('**' + message.author.username + '**, please don\'t say that !')
-    }
-  })
+client.on("message", (message) => {
+  // verifies if the message is sent by a user
+  if (message.author.bot) return;
 
-  // verifies if the message start with the prefix and is sent by a user
-  if (!message.content.startsWith(prefix) || message.author.bot) return
+  // delete a message if it contains a forbidden word
+  forbiddenWords.forEach((word) => {
+    if (message.content.toLowerCase() === word) {
+      message.delete();
+      message.channel.send(
+        "**" + message.author.username + "**, please don't say that !"
+      );
+    }
+  });
+
+  // verifies if the message start with the prefix
+  if (!message.content.startsWith(prefix)) return;
 
   // split the message to get the command
-  const args = message.content.slice(prefix.length).split(/ +/)
-  const command = args.shift().toLowerCase()
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
   // verifies if the command exists
-  if (!client.commands.has(command)) return
+  if (!client.commands.has(command)) return;
 
   // tries to run the command
   try {
-    client.commands.get(command).execute(message, args)
+    client.commands.get(command).execute(message, args);
   } catch (e) {
-    console.error(e)
-    message.reply('Une erreur s\'est produite.')
+    console.error(e);
+    message.lineReply("An error has occured.");
   }
-})
+});
 
 // logs the bot
-client.login(token)
+client.login(token);
 
 // sets the status
-client.on('ready', () => {
-  console.log('Connected as ' + client.user.tag)
-  client.user.setActivity('the help page | ;help', { status: 'online', type: 'WATCHING' })
-})
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag);
+  client.user.setActivity("you", { status: "online", type: "WATCHING" });
+});
